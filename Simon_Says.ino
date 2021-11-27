4fcddc79f9be0c5d1e25b8f467315b55e39aeb9f
jsiegel-cn <joshsaddress@gmail.com>
2021-11-27 16:04:33 -0500

    more random

--- Simon_Says.ino
+++ Simon_Says.ino
@@ -13,7 +13,7 @@ const int YELLOWB = 11;
 const unsigned long INPUTTIME = 5000;
 int order[100];
 int i = 0;
-unsigned long TIME = 0;   // MUST NOT BE int - goes negative
+unsigned long TIME = 0; // MUST NOT BE int - goes negative
 bool LOST = false;
 
 void setup()
@@ -25,25 +25,30 @@ void setup()
   pinMode(BLUE, OUTPUT);
   pinMode(YELLOW, OUTPUT);
   pinMode(PIEZO, OUTPUT);
-  pinMode(REDB, INPUT_PULLUP);      // Makes it HIGH to start so we can test for LOW
+  pinMode(REDB, INPUT_PULLUP); // Makes it HIGH to start so we can test for LOW
   pinMode(GREENB, INPUT_PULLUP);
   pinMode(BLUEB, INPUT_PULLUP);
   pinMode(YELLOWB, INPUT_PULLUP);
-  randomSeed(analogRead(0));
+  randomSeed(analogRead(0) * analogRead(1));
   Serial.println("STARTING");
 }
 
 void loop()
 {
-  if (LOST) {
-    delay(10000000000);  // Press Arduino reset to restart
+  Serial.println("Starting Loop");
+  if (LOST)
+  {
+    delay(10000000000); // Press Arduino reset to restart
   }
-  while (true) {
+  while (true)
+  {
     playSequence();
     TIME = millis();
-    for (int j=0; j < i; j++) {
-      delay(100);
-      if (!userInput(order[j])) {
+    for (int j = 0; j < i; j++)
+    {
+      delay(50);
+      if (!userInput(order[j]))
+      {
         Serial.println("LOST");
         LOST = true;
         sorry();
@@ -53,72 +58,91 @@ void loop()
   }
 }
 
-void sorry() {
-  for (int j=600; j>100; j-=5){
+void sorry()
+{
+  for (int j = 600; j > 100; j -= 5)
+  {
     tone(PIEZO, j);
     delay(10);
   }
   noTone(PIEZO);
 }
 
-void playTone(int pin) {
+void playTone(int pin)
+{
   digitalWrite(pin, HIGH);
-      tone(PIEZO, pin*100);
-      delay(400);
-      digitalWrite(pin, LOW);
-      noTone(PIEZO);
-      delay(200);
+  tone(PIEZO, pin * 100);
+  delay(400);
+  digitalWrite(pin, LOW);
+  noTone(PIEZO);
+  delay(200);
 }
 
-void playSequence() {
-    delay(1000);
-    order[i] = random(RED, YELLOW+1);
-    for (int j=0; j <= i; j++) {
-      playTone(order[j]);
-    }
-    i++;
+void playSequence()
+{
+  delay(1000);
+  order[i] = random(RED, YELLOW + 1);
+  for (int j = 0; j <= i; j++)
+  {
+    playTone(order[j]);
+  }
+  i++;
 }
 
-
-bool userInput(int light) {
-  while (true) {
-    if (millis() >= TIME + INPUTTIME + i*1000) {
+bool userInput(int light)
+{
+  while (true)
+  {
+    if (millis() >= TIME + INPUTTIME + i * 1000)
+    {
       Serial.println("TIME UP");
       return false;
     }
-    if (digitalRead(REDB) == LOW) {
+    if (digitalRead(REDB) == LOW)
+    {
       playTone(RED);
-      if (light == RED) {
+      if (light == RED)
+      {
         return true;
       }
-      else {
+      else
+      {
         return false;
       }
     }
-    if (digitalRead(GREENB) == LOW) {
+    if (digitalRead(GREENB) == LOW)
+    {
       playTone(GREEN);
-      if (light == GREEN) {
+      if (light == GREEN)
+      {
         return true;
       }
-      else {
+      else
+      {
         return false;
       }
     }
-    if (digitalRead(BLUEB) == LOW) {
+    if (digitalRead(BLUEB) == LOW)
+    {
       playTone(BLUE);
-      if (light == BLUE) {
+      if (light == BLUE)
+      {
         return true;
       }
-      else {
+      else
+      {
         return false;
       }
     }
-    if (digitalRead(YELLOWB) == LOW) {
+    if (digitalRead(YELLOWB) == LOW)
+    {
       playTone(YELLOW);
-      if (light == YELLOW) {
+      if (light == YELLOW)
+      {
         return true;
       }
-      else {
+      else
+      {
         return false;
       }
     }
